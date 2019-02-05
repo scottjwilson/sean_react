e971e1057dd872c01445d9957f81c4bd1ce8937a
Scott Wilson <slimzel@protonmail.com>
2019-02-04 16:30:51 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Messages from './components/Messages';
+import './App.css'
 
 class App extends Component {
+  state = {
+    messages: [
+    {
+      id: 1,
+      tite: 'Lebron is trash'
+    },
+    {
+      id: 2,
+      tite: 'And so are the Lakers'
+    },
+    {
+      id: 3,
+      tite: 'Not as bad as the Cowboys though...'
+    }
+    ]
+  }
+
   render() {
+    console.log(this.state.messages)
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Sean App</h1>
+        <Messages />
       </div>
     );
   }
---
+++ src/components/Messages.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Messages extends Component {
+  render() {
+    return (
+      <div>
+        <h4>Messages</h4>
+      </div>
+    );
+  }
+}
+
+export default Messages;
